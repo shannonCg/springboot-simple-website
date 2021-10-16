aba19e2589b93766cabf80f0f49f25c04b275f7c
shannonCg <phy8233@gmail.com>
2021-10-16 14:10:19 +0800

    refactor

--- src/main/java/app/controller/HeroController.java
+++ src/main/java/app/controller/HeroController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import app.Application;
 import app.model.to.HeroTO;
 import app.model.vo.HeroVO;
 import app.service.HeroService;
